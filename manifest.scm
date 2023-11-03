21e1f4a736caf7ea86b2487dd0d6ccbbfaef9620
陈功 <chengong456@qq.com>
2023-11-02 21:40:15 -0700

    x86 building

--- manifest.scm
+++ manifest.scm
@@ -8,4 +8,5 @@
   "libgccjit"
   "clang-toolchain"
   "dtc"
+  "grub"
 ))
--- src/arch/x86_64/mod.rs
+++ src/arch/x86_64/mod.rs
@@ -1,1 +1,20 @@
+pub mod entry;
 
+lazy_static::lazy_static! {
+    pub static ref WRITER: spin::Mutex<crate::uart::UartWriter> = todo!();
+    pub static ref ALLOCATOR: spin::Mutex<crate::heap_alloc::AndyAllocator<4096>> = todo!();
+}
+
+pub fn abort() -> ! {
+    unsafe {
+        core::arch::asm!("cli");
+        loop {
+            core::arch::asm!("hlt");
+        }
+    }
+}
+
+#[no_mangle]
+pub fn kinit() {
+    todo!()
+}
