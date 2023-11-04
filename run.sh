3f379bc0e68b8250f76e49c5389c87f4ab05fdaf
陈功 <chengong456@qq.com>
2023-11-03 19:25:55 -0700

    scripts fix

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-qemu-system-riscv64 -machine virt -bios none -serial mon:stdio -nographic -kernel ./target/riscv64gc-unknown-none-elf/debug/chad_os
+qemu-system-riscv64 -machine virt -bios none -serial mon:stdio -nographic -kernel ./target/riscv64gc-unknown-none-elf/debug/kernel
--- scripts/run_debug.sh
+++ scripts/run_debug.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-../../../toolchain_bins/bin/riscv64-unknown-elf-gdb --se=../target/riscv64gc-unknown-none-elf/debug/chad_os --eval-command="target remote localhost:1234"
+../../../toolchain_bins/bin/riscv64-unknown-elf-gdb --se=../target/riscv64gc-unknown-none-elf/debug/kernel --eval-command="target remote localhost:1234"
