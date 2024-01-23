7b2a90edc0a5677ddf8b6ed3eec11cdebacb872b
陈功 <chengong456@qq.com>
2024-01-23 15:24:43 -0800

    fix script

--- scripts/x86_64/run.sh
+++ scripts/x86_64/run.sh
@@ -7,5 +7,5 @@ cp ../../target/x86_64-unknown-uefi/debug/bootloader.efi ./esp/efi/boot/bootx64.
 printf "hihi\n你好\b" > ./esp/poo.txt
 qemu-system-x86_64 \
     -enable-kvm \
-    -drive if=pflash,format=raw,readonly=on,file=/gnu/store/dk4m2z88bhfwj6m4s2jmz3nd2hbnc7q0-ovmf-20170116-1.13a50a6/share/firmware/ovmf_x64.bin \
+    -drive if=pflash,format=raw,readonly=on,file=$HOME/.guix-home/profile/share/firmware/ovmf_x64.bin  \
     -drive format=raw,file=fat:rw:esp
